9d1c187a917b22534f745ec3d0cf887efba49ef5
sayurlearning <sayurlearning@gmail.com>
2023-09-01 09:11:31 -0700

    added more

--- Programming Exercise/ForAndWhile/BreakAndContinue.py
+++ Programming Exercise/ForAndWhile/BreakAndContinue.py
@@ -68,4 +68,7 @@ for month, phoneCount in enumerate(monthlySalesList):
     previousMonthSalary = currentMonthSalary #Why are we doing this?
 
  
-     
+   ##### Problem 4 ###
+   # Same as above. When the cumulative bonus is more than one lakh , base salary is increased
+   # by 5% for each month. Calculate the salary for each month.
+   
