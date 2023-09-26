b28b82f9471a37631ae9fdfe51a7fb0d4d141b5e
sayurlearning <sayurlearning@gmail.com>
2023-09-25 19:21:41 -0700

    new problems

---
+++ Programming Exercise/LogicalThinking/ParkingGarage.py
@@ -0,0 +1,23 @@
+'''
+Parking garage problem
+1.Write a program to calculate the revenue generated in a parking garage in a shopping mall
+
+Parking fee is 
+If you return within 15 mins, its free
+Rs 100 for the first hr
+Rs 150 for each hr after that. 
+Fee is calculated in 30 min increments. (meaning, if you spent 25 mins, you will be charged for 30 mins
+If you spend 35 mins, you will be charged for one hr)
+Get entry time and exit time from customer as input and display the fee.
+
+2.same as above. End the program if 24hrs passes from when the first customer comes in. 
+Print the total fees collected.
+
+3. Same as above, adding more condition.
+The parking garage has 10 spaces numbered from 1 to 10.
+THere is a car entering or exiting every 10 mins.
+When a car enters, you need to tell them which lot is allotted for them.
+When the customer comes to pick up the car, get the lot number as input and calculate the fees.
+
+Use arrays if needed.
+'''
---
+++ Programming Exercise/LogicalThinking/SimpleGames.py
@@ -0,0 +1,12 @@
+'''
+Games
+
+Problem #1
+Play the game Rock Papar Scissors with the computer.
+Play it three times and best of three wins. 
+If you win, print your name in color (look for a python package to do this)
+Hint - Use random number generation
+
+
+
+'''
