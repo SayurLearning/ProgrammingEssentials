0ebf4957c28500a3f87dd85fe80732485ce6eba2
sayurlearning <sayurlearning@gmail.com>
2023-09-26 21:27:56 -0700

    more

--- Programming Exercise/LogicalThinking/SimpleGames.py
+++ Programming Exercise/LogicalThinking/SimpleGames.py
@@ -7,6 +7,21 @@ Play it three times and best of three wins.
 If you win, print your name in color (look for a python package to do this)
 Hint - Use random number generation
 
+Problem #2
+Two player dice game.
+Each player will roll the die (numbers from 1 to 6)
+Points are added to each roll.
+1 - 1 pt
+2 - 5 pts
+3 - 15 pts
+4 - (-15) pts
+5 - (-5) pts
+6 - (-1) pts
+
+Find which player scores 100 first and how many time they had to roll the die.
+Hint - use random numbers to generate the die (no need to get user input)
+Use Arrays and while loop.
 
 
+ 
 '''
