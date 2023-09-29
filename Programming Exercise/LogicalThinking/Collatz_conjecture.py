732038fc271a23826a2d19abecb0442db0aff063
sayurlearning <sayurlearning@gmail.com>
2023-09-29 16:12:20 -0700

    more

---
+++ Programming Exercise/LogicalThinking/Collatz_conjecture.py
@@ -0,0 +1,23 @@
+'''
+Problem #1
+Write a program for Collatz_conjecture.
+Collatz_conjecture means -  start with the input number. 
+For even number , divide by 2 (n/2)
+For odd number - 3n + 1
+apply the above for the result number until the answer is 1.
+
+Eg, input is 8
+8, 4,2, 1
+input is 9
+9,28,14,7,22,11,34,17,52,26,13,40,20,10,5,16,8,4,2,1
+
+Problem #2
+Same as above.
+Input two numbers.
+Print which number has less steps to reach 1.
+
+#Problem #3
+Get a input. Create a sequence of numbers from that input using the above alg.
+Find the largest number in the sequence. 
+
+'''
