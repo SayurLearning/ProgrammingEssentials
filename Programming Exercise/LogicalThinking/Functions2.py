b685c3a89ea996465a7d58e4e9df76edd3908094
sayurlearning <sayurlearning@gmail.com>
2023-10-02 18:36:16 -0700

    New problem

---
+++ Programming Exercise/LogicalThinking/Functions2.py
@@ -0,0 +1,16 @@
+"""
+Problem 1
+In the input, find the first A and last A, print all the letters between these two A.
+
+Problem 2:
+  In the input, find the first A and last A, print all the letters between these two A. 
+  If there is no A or 2nd A is not found, find the first B  and last B and print all the letters between these two B. 
+  If there is no B or 2nd B is not found, find the first C and last C and print all the letters between these two C. 
+
+Problem 3:
+  In the input, find the first A and last A, print all the letters between these two A. 
+  If there is no A or 2nd A is not found, find the first B after the first A (if there is a A) and last B and print all the letters between these two B. 
+  
+  If there is no B or 2nd B is not found, find the first C after the first B (if there is a B) and last C and print all the letters between these two C. 
+
+"""
