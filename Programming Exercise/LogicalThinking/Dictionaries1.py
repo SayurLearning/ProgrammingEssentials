e9838996aecb590835ec92a9ce36d677a152f0e3
sayurlearning <sayurlearning@gmail.com>
2023-10-09 17:40:54 -0700

    New problems

---
+++ Programming Exercise/LogicalThinking/Dictionaries1.py
@@ -0,0 +1,24 @@
+"""
+Problem #1
+Write the dictionaty structure for storing recipes for food.
+Hardcode your own data.
+Get user input for the name of the recipe, and then print the recipe. 
+Eg: Input : Idli
+Output : Here is the recipe for idli. 
+You need ingrediants Rice and Dal.
+ The procedure to cook is grind rice and dal, ferment and steam.
+Input : Yogurt rice
+Output :  Here is the recipe for Yogurt rice. 
+You need ingrediants Rice and yogurt.
+The procedure to cook is cook rice first and then add yogurt.
+
+
+Problem #2
+Write the dictionaty structure for storing recipes for food.
+Hardcode your own data.
+User input is any ingredient. List all the recipes that include the ingredient
+Input : Rice
+Output : You can make Idli and Yogurt rice with rice.
+
+
+"""
